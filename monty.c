ddb2965ce927a0a20404f90475b9170c46816602
SANTIAGOH22 <hinestrozasantiago@gmail.com>
2023-05-19 09:16:36 -0700

    add the monty project

--- monty.c
+++ monty.c
@@ -1,47 +1,84 @@
 #include "monty.h"
 
-int error = 0;
+char *line = NULL;
 
 /**
- * main - main monty program to handle the bytecode files
- * @argc: argument count
- * @argv: array of argument
- * Return: EXIT_SUCCESS for success, EXIT_FAILURE for failure
+ * main - Executes de commands of a monty file
+ * @argc: Size of argv
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 1 on failure
  */
-
 int main(int argc, char **argv)
 {
-	FILE *fd;
-	stack_t *stack = NULL;
-	unsigned int line_number = 0;
-	char str[1024];
-	char *tok = NULL;
-	size_t n = 1024;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+    unsigned int line_number = 1;
+    unsigned int var;
+    char *command = NULL;
+    
+    stack_t *stack = NULL;
 
-	fd = fopen(argv[1], "r");
-	if (fd == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (fgets(str, n, fd) != NULL && error != 1)
-	{
-		line_number++;
-		tok = strtok(str, "\n\t ");
-		if (tok != NULL)
-		{
-			check_opcode(tok, &stack, line_number);
-		}
-	}
-	free_all(stack, fd);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if (error == 1)
-		exit(EXIT_FAILURE);
-	return (EXIT_SUCCESS);
+    while((line = lines_reader(argv[1], line_number)) != NULL)
+    {
+        /* printf("%s\n", line); */
+        
+        command = strtok(line, " \r\t\n");
+        if (command == NULL)
+        {
+            line_number++;
+            free(line);
+            continue;
+        }
+
+        if (strcmp(command, "push") == 0)
+        {
+            var = 1;
+        }
+        else if (strcmp(command, "pall") == 0)
+        {
+            var = 2;
+        }
+        else 
+        {
+            var = 0;
+        }
+
+
+    switch(var)
+    {
+        case 1:
+            push(&stack, line_number);
+            break;
+
+        case 2:
+            pall(&stack, line_number);
+            break;
+
+        // operator doesn't match any case constant +, -, *, /
+        default:
+            dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, command);
+            exit(EXIT_FAILURE);
+    }
+        /*
+
+        if (strcmp(command, "nop") == 0)
+        {
+            nop(&stack, line_number);
+        }
+
+        */
+        
+        free(line);
+        line_number++;
+    }
+
+    stack_freer(stack);
+
+    return (EXIT_SUCCESS);
 }
